8c01e4106e1d911d0c80f838174caec9c0cc2dd1
deadxraver <409853@edu.itmo.ru>
2025-03-18 12:50:14 +0300

    program is working ok, but has troubles with big prime numbers (solved by creating my own sqrt?)

--- risc-iv/is_prime.s
+++ risc-iv/is_prime.s
@@ -2,8 +2,44 @@
 input_addr:   .word 0x80
 output_addr:  .word 0x84
 
-  .text 
+  .text
 
 .org 0x85
 _start:
+  ; t0 - input_addr
+  lui       t0, %hi(input_addr)             ; higher byte to higher byte
+  addi      t0, t0, %lo(input_addr)         ; lower byte to lower byte
+  lw        t0, 0(t0)                       ; load the input_addr itself
+  ; t0 - input number, t1 - divisor, t2 - remainder, t3 - max divisor (for loop)
+  lw        t0, 0(t0)                       ; load value from input
+  xor       t1, t1, t1                      ; t1 = 0
+  ble       t0, t1, error                   ; n < 1 ? goto error
+  addi      t1, t1, 2                       ; t1 = 2
+  bgt       t1, t0, not_prime               ; n < 2 ? goto not_prime
+  beq       t1, t0, is_prime
+  div       t3, t0, t1                      ; t3 = n / 2
+  addi      t3, t3, 1                       ; t3 = n / 2 + 1
+loop:
+  ; t2 = t0 % t1; t2 ? goto not_prime; t1++; t1 >= t3 ? goto is_prime
+  rem       t2, t0, t1
+  beqz      t2, not_prime
+  addi      t1, t1, 1
+  ble       t3, t1, is_prime
+  j         loop
+error:
+  xor       t1, t1, t1
+  addi      t1, t1, -1
+  j         print_res
+is_prime:
+  xor       t1, t1, t1
+  addi      t1, t1, 1
+  j         print_res
+not_prime:
+  xor       t1, t1, t1
+print_res:
+  lui       t0, %hi(output_addr)
+  addi      t0, t0, %lo(output_addr)
+  lw        t0, 0(t0)
+  sw        t1, 0(t0)
   halt
+
---
+++ risc-iv/run.sh
@@ -0,0 +1,1 @@
+wrench is_prime.s --isa risc-iv-32 -c is_prime_11.yaml
