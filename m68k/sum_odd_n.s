d9ff6acb3af095d72595271a0e70d1c5b38574cc
deadxraver <409853@edu.itmo.ru>
2025-06-23 17:34:06 +0300

    идиотские требования, неприменимые к моему варианту, зачем вообще вложенные вызовы там, где сохранение адреса возврата происходит автоматически?

--- m68k/sum_odd_n.s
+++ m68k/sum_odd_n.s
@@ -16,8 +16,29 @@ f_sum_odd_n:
   add.l         1, D0           ; ++n
   lsr.l         1, D0           ; n /= 2
   mul.l         D0, D0          ; n**2
-  bvs error_overflow
+  bvs           error_overflow
   rts
+
+main:
+  link          A6, -4          ; create stack frame
+  move.l        D0, -4(A6)      ; save value from outer scope to stack
+  move.l        (A0), D0        ; read n
+  beq           error_range     ; if n == 0
+  bmi           error_range     ; if n < 0
+
+  jsr           f_sum_odd_n     ; call
+ 
+  move.l        D0, (A1)        ; print
+  move.l        -4(A6), D0      ; return old D0
+  unlk          A6              ; destroy stack frame
+  rts
+
+  .data
+.org 0x80
+input_port:     .word 0
+output_port:    .word 0
+
+  .text
 
 _start:
   movea.l       stack_top, A7
@@ -27,11 +48,5 @@ _start:
   movea.l       output_addr, A1
   movea.l       (A1), A1        ; output_addr to A1
 
-  move.l        (A0), D0        ; read n
-  beq           error_range     ; if n == 0
-  bmi           error_range     ; if n < 0
-
-  jsr           f_sum_odd_n     ; call
- 
-  move.l        D0, (A1)
+  jsr           main            ; call just for call
   halt
